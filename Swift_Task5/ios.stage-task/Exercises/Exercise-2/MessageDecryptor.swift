288ecff0d7b3cf35daffdecd8c44679c09e5eb3e
Alexey Sur <alexeysur@gmail.com>
2021-06-14 21:51:35 +0300

    feat: solution for exercice 2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,53 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var countingArray: [String] = []
+        var tempCountString = ""
+        var repeatStringArray: [String] = []
+        var result = ""
+        
+        if message.contains(" ") {
+            return ""
+        }
+        
+        if message.count <= 1 || message.count >= 60 {
+            return ""
+        }
+        
+        let stringArray = message.components(separatedBy: CharacterSet.decimalDigits.inverted)
+        for item in stringArray {
+            if let number = Int(item) {
+                if !(1...300).contains(number) { return "" }
+            }
+        }
+        
+        
+        for character in message {
+            if character.isNumber {
+                tempCountString = "\(tempCountString)\(character)"
+            } else if character == "[" {
+                tempCountString == "" ? countingArray.append("1") : countingArray.append(tempCountString)
+                tempCountString = ""
+                
+                repeatStringArray.append(result)
+                result = ""
+            } else if character == "]" {
+                var tempStr = ""
+                
+                if let counts = Int(countingArray.popLast() ?? "1") {
+                    for _ in 0..<counts {
+                        tempStr += result
+                    }
+                }
+                guard let popLastOfRepeatString = repeatStringArray.popLast() else { return "" }
+                
+                result = popLastOfRepeatString + tempStr
+            } else if character.isLetter {
+                if ("A"..."Z").contains(character) { return "" }
+                result += String(character)
+            }
+        }
+        
+        return result
     }
 }
