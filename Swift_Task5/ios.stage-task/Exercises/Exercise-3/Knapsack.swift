3e5bb54bf454ceec97c9153b4eac9b0173a7a0b7
Alexey Sur <alexeysur@gmail.com>
2021-06-14 22:14:50 +0300

    feat: part solution for exercice 3. Not all tests successful

--- Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -17,6 +17,40 @@ public final class Knapsack {
     }
     
     func findMaxKilometres() -> Int {
-        return -1
+        var foodAndDrinkArray: [[Int]] = []
+        var bestDistanse = 0
+        
+        guard maxWeight > 0 && maxWeight <= 2500 else {
+            return 0
+        }
+        
+        if drinks.count < 1 || drinks.count > 100 { return 0 }
+        if foods.count < 1 || foods.count > 100 { return 0 }
+        
+        for food in foods {
+            for drink in drinks {
+                if (food.weight + drink.weight) <= maxWeight {
+                    var tmpFoodAndDrink: [Int] = []
+                    tmpFoodAndDrink.append(food.value)
+                    
+                    tmpFoodAndDrink.append(drink.value)
+                    foodAndDrinkArray.append(tmpFoodAndDrink)
+                }
+            }
+        }
+        var tmpArray: [Int] = []
+        
+        for i in foodAndDrinkArray {
+            i.forEach( {_ in
+                if let minValue = i.min() {
+                    tmpArray.append(minValue)
+                }
+            })
+        }
+        
+        bestDistanse = tmpArray.max() ?? 0
+        
+        return bestDistanse
     }
+    
 }
